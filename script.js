21abe97ce9e620a2960c933ce5fe77be6de74aac
dance01 <dani.ceva96@gmail.com>
2025-02-17 00:15:50 +0100

    Add UI and delete playGame function
    Add 3 buttons for user choices and link them to
    playRound function. I delete the playGame function
    to just work on the UI.

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
-alert("Welcome to my Rock Scissor Paper\nIt's five rounds\nLet's go!")
-playGame();
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+rock.addEventListener("click", () => playRound('rock'));
+paper.addEventListener("click", () => playRound('paper'));
+scissors.addEventListener("click", () => playRound('scissors'));
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+let humanScore = 0;
+let computerScore = 0;
+function playRound(hSelection){
+    
+    let humanSelection = hSelection;
+    let computerSelection = getRandomInt(3);
 
-    function playRound(){
-        let humanSelection = prompt("rock, paper or scissor?");
-        let computerSelection = getRandomInt(3);
+    switch (humanSelection){
+        case "rock":
+            humanSelection = 0;
+            console.log("YOU: rock");
+            break;
+        case "paper":
+            humanSelection = 1;
+            console.log("YOU: paper");
+            break;
+        case "scissors":
+            console.log("YOU: scissors");
+            humanSelection = 2;
+            break;
+    };
 
-        switch (humanSelection.toLowerCase()){
-            case "rock":
-                humanSelection = 0;
-                console.log("YOU: rock");
-                break;
-            case "paper":
-                humanSelection = 1;
-                console.log("YOU: paper");
-                break;
-            case "scissor":
-                console.log("YOU: scissor");
-                humanSelection = 2;
-                break;
-        };
+    if (computerSelection === 0)
+        console.log("Computer: rock");
+    else if (computerSelection === 1)
+        console.log("Computer: paper");
+    else if (computerSelection === 2)
+        console.log("Computer: scissors");
 
-        if (computerSelection === 0)
-            console.log("Computer: rock");
-        else if (computerSelection === 1)
-            console.log("Computer: paper");
-        else if (computerSelection === 2)
-            console.log("Computer: scissor");
-
-        if (humanSelection === computerSelection){
-            console.log("pair!");
-        }
-        else if (humanSelection === 0 && computerSelection === 2
-                || humanSelection === 1 && computerSelection === 0
-                || humanSelection === 2 && computerSelection === 1){
-            console.log("Win!");
-            humanScore ++;
-        }
-        else {
-            console.log("Lose!");
-            computerScore ++;
-        }
-
-        console.log("Your Point: ", humanScore);
-        console.log("Computer Point: ", computerScore);
+    if (humanSelection === computerSelection){
+        console.log("pair!");
     }
-if (humanScore > computerScore)
-    alert(`YOU WON! ${humanScore} to ${computerScore}`)
-else if (humanScore < computerScore)
-    alert(`YOU LOST! ${humanScore} to ${computerScore}`)
-else alert(`PAIR! ${humanScore} to ${computerScore}`)
+    else if (humanSelection === 0 && computerSelection === 2
+            || humanSelection === 1 && computerSelection === 0
+            || humanSelection === 2 && computerSelection === 1){
+        console.log("Win!");
+        humanScore ++;
+    }
+    else {
+        console.log("Lose!");
+        computerScore ++;
+    }
+
+    console.log("Your Point: ", humanScore);
+    console.log("Computer Point: ", computerScore);
 }
+// if (humanScore > computerScore)
+//     alert(`YOU WON! ${humanScore} to ${computerScore}`)
+// else if (humanScore < computerScore)
+//     alert(`YOU LOST! ${humanScore} to ${computerScore}`)
+// else alert(`PAIR! ${humanScore} to ${computerScore}`)
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+
